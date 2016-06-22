1144a24154a229c06f082ca11fed20d7788d9599
Ernesto Mainegra-Hing <ernesto.mainegra-hing@nrc-cnrc.gc.ca>
2016-06-22 14:39:33 -0400

    Remove duplicated logical blocks
    Method getPositionDirection had duplicated if-blocks.

--- HEN_HOUSE/egs++/sources/egs_isotropic_source/egs_isotropic_source.h
+++ HEN_HOUSE/egs++/sources/egs_isotropic_source/egs_isotropic_source.h
@@ -155,8 +155,13 @@ public:
         do {
             x = shape->getRandomPoint(rndm);
             if( geom ) {
-                if( gc == IncludeAll ) ok = geom->isInside(x);
-                else if( gc == ExcludeAll ) ok = !geom->isInside(x);
+                if( gc == IncludeAll ) {
+                   ok = geom->isInside(x);
+                }
+                else if( gc == ExcludeAll ) {
+                     ok = !geom->isInside(x);
+                  
+                }
                 else if( gc == IncludeSelected ) {
                     ok = false;
                     int ireg = geom->isWhere(x);
@@ -166,32 +171,20 @@ public:
                             break;
                         }
                     }
-                    else if (gc == ExcludeAll) {
-                        ok = !geom->isInside(x);
-                    }
-                    else if (gc == IncludeSelected) {
-                        ok = false;
-                        int ireg = geom->isWhere(x);
-                        for (int j=0; j<nrs; ++j) {
-                            if (ireg == regions[j]) {
-                                ok = true;
-                                break;
-                            }
-                        }
-                    }
-                    else {
-                        ok = true;
-                        int ireg = geom->isWhere(x);
-                        for (int j=0; j<nrs; ++j) {
-                            if (ireg == regions[j]) {
-                                ok = false;
-                                break;
-                            }
-                        }
-                    }
+                }
+                else {
+                     ok = true;
+                     int ireg = geom->isWhere(x);
+                     for (int j=0; j<nrs; ++j) {
+                         if (ireg == regions[j]) {
+                             ok = false;
+                             break;
+                         }
+                     }
                 }
             }
-        } while ( !ok );
+        }
+        while ( !ok );
 
         u.z = rndm->getUniform()*(buf_1 - buf_2) - buf_1;
 
